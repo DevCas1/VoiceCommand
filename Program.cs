10c447c78d92f9c914cae8bc40fee23b39681ee6
DevCas1 <18188660+DevCas1@users.noreply.github.com>
2021-11-14 16:13:43 +0100

    More stuff

--- Program.cs
+++ Program.cs
@@ -1,21 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Speech.Recognition;
+using System.Speech.Recognition.SrgsGrammar ;
 
 namespace VoiceCommand
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args) // Add (string[] args) to use command line arguments when starting the program
         {
-            var recognizer = new SpeechRecognizer();
-            //var grammar = new Grammar(GrammarBuilder.Add(new Choices(), );
-            //recognizer.LoadGrammarAsync(grammar);
+            SpeechRecognizer recognizer = new SpeechRecognizer();
+            recognizer.SpeechRecognized += OnSpeechRecognized;
+
+            SrgsRule moduleRule = new SrgsRule("moduleList");
+
+            SrgsOneOf modules = new SrgsOneOf(new string[] { "engine", "weapon", "shield"});
+            moduleRule.Add(modules);
+            moduleRule.Scope = SrgsRuleScope.Public;
+
+            SrgsDocument document = new SrgsDocument();
+            document.Rules.Add(moduleRule);
+            document.Root = moduleRule;
+
+            SrgsItem[] optionalItems = { new SrgsItem(0, 1, "the"), new SrgsItem(0, 1, "the") };
+            SrgsItem mandatoryItem = new SrgsItem(1, 1, "power");
+            SrgsItem alternativeMandatoryItem = new SrgsItem(1, 1, "convert");
 
             Console.ReadLine();
+        }
+
+        private static void OnSpeechRecognized(SpeechRecognizedEventArgs args)
+        {
+
         }
     }
 }
