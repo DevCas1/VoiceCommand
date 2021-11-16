9fcc5def8925cd746b74d23bb183eb9290104a0d
DevCas1 <18188660+DevCas1@users.noreply.github.com>
2021-11-16 09:07:11 +0100

    Add general shut-down word for closing the program

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace VoiceCommand
     internal class Program
     {
         private static ManualResetEvent Completed = null;
+        private static string ShutdownWord = "Close Voice Command";
 
         private static void Main(string[] args) // Add (string[] args) to use command line arguments when starting the program
         {
@@ -20,8 +21,17 @@ namespace VoiceCommand
             recognitionEngine.SpeechRecognized += OnSpeechRecognized;
             recognitionEngine.SpeechRecognitionRejected += OnSpeechRecognitionRejected;
 
+            recognitionEngine.LoadGrammar(new Grammar(new GrammarBuilder(ShutdownWord)));
             recognitionEngine.LoadGrammar(new Grammar(new GrammarBuilder("test")));
-            recognitionEngine.LoadGrammar(new Grammar(new GrammarBuilder("exit")));
+            recognitionEngine.LoadGrammar(new Grammar(new GrammarBuilder("power the engines")));
+            recognitionEngine.LoadGrammar(new Grammar(new GrammarBuilder("power the weapons")));
+            recognitionEngine.LoadGrammar(new Grammar(new GrammarBuilder("power the shields")));
+            recognitionEngine.LoadGrammar(new Grammar(new GrammarBuilder("maximize engines")));
+            recognitionEngine.LoadGrammar(new Grammar(new GrammarBuilder("maximize weapons")));
+            recognitionEngine.LoadGrammar(new Grammar(new GrammarBuilder("maximize shields")));
+            //recognitionEngine.LoadGrammar(new Grammar(new GrammarBuilder("maximize shields"))); // Special setup
+            //recognitionEngine.LoadGrammar(new Grammar(new GrammarBuilder("maximize shields"))); // Special setup
+            //recognitionEngine.LoadGrammar(new Grammar(new GrammarBuilder("maximize shields"))); // Special setup
 
             recognitionEngine.SetInputToDefaultAudioDevice(); // set the input of the speech recognizer to the default audio device
             recognitionEngine.RecognizeAsync(RecognizeMode.Multiple); // recognize speech asynchronous
@@ -30,15 +40,20 @@ namespace VoiceCommand
 
             recognitionEngine.Dispose(); // dispose the speech recognition engine
 
-            Console.ReadLine();
+            //Console.ReadLine();
         }
 
         private static void OnSpeechRecognized(object sender, SpeechRecognizedEventArgs args)
-        {
-            Console.WriteLine($"Recognized: {args.Result.Text}");
-            
-            if (args.Result.Text == "exit")
+        {            
+            if (args.Result.Text == ShutdownWord)
+            {
+                Console.WriteLine("Shutdown word recognized, shutting down...");
+                Thread.Sleep(1000);
                 Completed.Set();
+                return;
+            }
+
+            Console.WriteLine($"Recognized: {args.Result.Text}");
         }
 
         private static void OnSpeechRecognitionRejected(object sender, SpeechRecognitionRejectedEventArgs args) => Console.WriteLine("Still listening...");
