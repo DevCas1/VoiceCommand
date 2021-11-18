7f87ff25a7ba1367675a52ef24b866593f333e8b
DevCas1 <18188660+DevCas1@users.noreply.github.com>
2021-11-18 21:21:27 +0100

    Rename InputSequence.cs to InputAction.cs | Remove InputSequence (is basically a System.Collections.Generic Queue<InputAction>)

---
+++ InputAction.cs
@@ -0,0 +1,17 @@
+﻿namespace VoiceCommand.Input
+{
+    /// <summary>A collection of Keyboard Scancode and the duration for it to hold.</summary>
+    public struct InputAction
+    {
+        public InputAction(Scancode scancode, bool keyDown, float duration)
+        {
+            Scancode = scancode;
+            KeyDown = keyDown;
+            Duration = duration;
+        }
+
+        public readonly Scancode Scancode;
+        public readonly bool KeyDown;
+        public readonly float Duration;
+    }
+}
