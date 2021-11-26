3e1cee5f9bf7cf02c8bf606125e6b24398969f20
DevCas1 <18188660+DevCas1@users.noreply.github.com>
2021-11-26 18:26:26 +0100

    Start with JSON serialization

---
+++ Serialization.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Windows;
+using System.Windows.Input;
+
+namespace VoiceCommand.Serialization
+{
+    internal class Serialization
+    {
+        /// <summary>
+        /// Load a list of predefined Commands from Json.
+        /// </summary>
+        /// <param name="commands">The list of Commands loaded from Json.</param>
+        /// <returns>Whether or not loading from Json succeeded.</returns>
+        public bool LoadCommandsFromJson(out Command[] commands)
+        {
+            commands = null;
+            //var dialog = new 
+            //var result = JsonSerializer.Deserialize<Command[]>()
+
+            return false;
+        }
+
+        /// <summary>
+        /// Save the supplied Command list to Json.
+        /// </summary>
+        /// <param name="commandList">The list of Commands to store.</param>
+        /// <returns>Whether the saving to Json succeeded.</returns>
+        public bool SaveToJson(Command[] commandList)
+        {
+            return false;
+        }
+    }
+}
