5b9ecfd200ab8a4aa939d139cada3dbe33be2646
Sjouke IJntema <18188660+DevCas1@users.noreply.github.com>
2024-07-26 10:47:03 +0200

    Target Windows specifically (MS SpeechRecognitionEngine unavailable on other platforms) | Add default LogType to LogToConsole() | Fix nullable type signature for events

--- VoiceCommand/Log.cs
+++ VoiceCommand/Log.cs
@@ -2,7 +2,7 @@
 {
     public static class Log
     {
-        public static void LogToConsole(string message, LogType logType) => Console.WriteLine($"{DateTime.Now} ({logType.ToString()}) {message}");
+        public static void LogToConsole(string message, LogType logType = LogType.Info) => Console.WriteLine($"[{DateTime.Now}] ({logType}) {message}");
 
         public enum LogType
         {
--- VoiceCommand/Program.cs
+++ VoiceCommand/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Speech.Recognition;
 using System.Threading;
+using VoiceCommand;
 using VoiceCommand.Input;
 using Scancode = VoiceCommand.Input.Scancode;
 
@@ -8,19 +9,19 @@ namespace VoiceCommandHagoeie
 {
     internal class Program
     {
-        private static ManualResetEvent Completed = null;
+        private static ManualResetEvent? Completed = null;
         private static string ShutdownWord = "Close Voice Command";
-        private static Command[] LoadedCommands = null;
+        private static Command[]? LoadedCommands = null;
 
         private static void Main() // Add (string[] args) to use command line arguments when starting the program
         {
-            Log.LogToConsole("Initializing...", Log.LogType.Info);
+            Log.LogToConsole("Initializing...");
 
             Completed = new ManualResetEvent(false);
 
             SpeechRecognitionEngine recognitionEngine = new SpeechRecognitionEngine();
 
-            recognitionEngine.LoadGrammarCompleted += (object o, LoadGrammarCompletedEventArgs a) => { Console.WriteLine("Grammars loaded."); };
+            recognitionEngine.LoadGrammarCompleted += static (object? sender, LoadGrammarCompletedEventArgs args) => Console.WriteLine("Grammars loaded.");
             recognitionEngine.SpeechRecognized += OnSpeechRecognized;
             recognitionEngine.SpeechRecognitionRejected += OnSpeechRecognitionRejected;
 
@@ -28,7 +29,7 @@ namespace VoiceCommandHagoeie
 
             recognitionEngine.SetInputToDefaultAudioDevice(); // set the input of the speech recognizer to the default audio device
             recognitionEngine.RecognizeAsync(RecognizeMode.Multiple); // recognize speech asynchronous
-            Log.LogToConsole("Ready!", Log.LogType.Info);
+            Log.LogToConsole("Ready!");
 
             Completed.WaitOne(); // wait until speech recognition is completed
             recognitionEngine.Dispose(); // dispose the speech recognition engine
@@ -38,54 +39,62 @@ namespace VoiceCommandHagoeie
         {
             Command[] commands = new Command[]
             {
-                new Command("test", new InputAction[]
-                {
-                    new InputAction(Scancode.sc_shiftLeft, true),
-                    new InputAction(Scancode.sc_h, true), new InputAction(Scancode.sc_h, false),
-                    new InputAction(Scancode.sc_shiftLeft, false),
-                    new InputAction(Scancode.sc_a, true), new InputAction(Scancode.sc_o, false),
-                    new InputAction(Scancode.sc_g, true), new InputAction(Scancode.sc_i, false),
-                    new InputAction(Scancode.sc_o, true), new InputAction(Scancode.sc_i, false),
-                    new InputAction(Scancode.sc_e, true), new InputAction(Scancode.sc_i, false),
-                    new InputAction(Scancode.sc_i, true), new InputAction(Scancode.sc_i, false),
-                    new InputAction(Scancode.sc_e, true), new InputAction(Scancode.sc_i, false),
-                }),       
+                new Command(
+                    "test", 
+                    [
+                        new InputAction(Scancode.sc_shiftLeft, true),
+                        new InputAction(Scancode.sc_h, true), new InputAction(Scancode.sc_h, false),
+                        new InputAction(Scancode.sc_shiftLeft, false),
+                        new InputAction(Scancode.sc_a, true), new InputAction(Scancode.sc_o, false),
+                        new InputAction(Scancode.sc_g, true), new InputAction(Scancode.sc_i, false),
+                        new InputAction(Scancode.sc_o, true), new InputAction(Scancode.sc_i, false),
+                        new InputAction(Scancode.sc_e, true), new InputAction(Scancode.sc_i, false),
+                        new InputAction(Scancode.sc_i, true), new InputAction(Scancode.sc_i, false),
+                        new InputAction(Scancode.sc_e, true), new InputAction(Scancode.sc_i, false),
+                    ]
+                ),       
                 
-                new Command("power engines", new InputAction[]
-                { 
-                    new InputAction(Scancode.sc_1, true), new InputAction(Scancode.sc_1, false),
-                }),        
+                new Command(
+                    "power engines", 
+                    [new InputAction(Scancode.sc_1, true), new InputAction(Scancode.sc_1, false),]
+                ),        
                 
-                new Command("power weapons", new InputAction[]
-                { 
-                    new InputAction(Scancode.sc_2, true), new InputAction(Scancode.sc_2, false),
-                }),        
+                new Command(
+                    "power weapons", 
+                    [new InputAction(Scancode.sc_2, true), new InputAction(Scancode.sc_2, false),]
+                ),        
                 
-                new Command("power shields", new InputAction[]
-                { 
-                    new InputAction(Scancode.sc_3, true), new InputAction(Scancode.sc_3, false),
-                }),
+                new Command(
+                    "power shields", 
+                    [new InputAction(Scancode.sc_3, true), new InputAction(Scancode.sc_3, false),]
+                ),
 
-                new Command("maximize engines", new InputAction[]
-                {
-                    new InputAction(Scancode.sc_controlLeft, true),
-                    new InputAction(Scancode.sc_1, true), new InputAction(Scancode.sc_1, false),
-                    new InputAction(Scancode.sc_controlLeft, false)
-                }),
+                new Command(
+                    "maximize engines", 
+                    [
+                        new InputAction(Scancode.sc_controlLeft, true),
+                        new InputAction(Scancode.sc_1, true), new InputAction(Scancode.sc_1, false),
+                        new InputAction(Scancode.sc_controlLeft, false)
+                    ]
+                ),
 
-                new Command("maximize weapons", new InputAction[]
-                {
-                    new InputAction(Scancode.sc_controlLeft, true),
-                    new InputAction(Scancode.sc_2, true), new InputAction(Scancode.sc_2, false),
-                    new InputAction(Scancode.sc_controlLeft, false)
-                }),
+                new Command(
+                    "maximize weapons", 
+                    [
+                        new InputAction(Scancode.sc_controlLeft, true),
+                        new InputAction(Scancode.sc_2, true), new InputAction(Scancode.sc_2, false),
+                        new InputAction(Scancode.sc_controlLeft, false)
+                    ]
+                ),
 
-                new Command("maximize shields", new InputAction[]
-                {
-                    new InputAction(Scancode.sc_controlLeft, true),
-                    new InputAction(Scancode.sc_3, true), new InputAction(Scancode.sc_3, false),
-                    new InputAction(Scancode.sc_controlLeft, false)
-                }),
+                new Command(
+                    "maximize shields", 
+                    [
+                        new InputAction(Scancode.sc_controlLeft, true),
+                        new InputAction(Scancode.sc_3, true), new InputAction(Scancode.sc_3, false),
+                        new InputAction(Scancode.sc_controlLeft, false)
+                    ]
+                ),
             };
 
             return commands;
@@ -94,16 +103,16 @@ namespace VoiceCommandHagoeie
         private static void AddGrammars(SpeechRecognitionEngine recognitionEngine, Command[] commands)
         {
             //new System.Globalization.CultureInfo("en-GB"); gr = new Grammar(grammarBuilder); // For SAPI errors regarding phonetic alphabet selection
-            Log.LogToConsole("Loading commands...", Log.LogType.Info);
+            Log.LogToConsole("Loading commands...");
             recognitionEngine.LoadGrammar(new Grammar(new GrammarBuilder(ShutdownWord)));
 
             foreach (var command in commands)
                 recognitionEngine.LoadGrammar(new Grammar(new GrammarBuilder(command.CommandPhrase)));
 
-            Log.LogToConsole($"{commands.Length} command{(commands.Length > 1 ? "s" : "")} loaded.", Log.LogType.Info);
+            Log.LogToConsole($"{commands.Length} command{(commands.Length > 1 ? "s" : "")} loaded.");
         }
 
-        private static void OnSpeechRecognized(object sender, SpeechRecognizedEventArgs args)
+        private static void OnSpeechRecognized(object? sender, SpeechRecognizedEventArgs args)
         {
             string result = args.Result.Text;
 
@@ -113,33 +122,37 @@ namespace VoiceCommandHagoeie
                 return;
             }
 
-            Command recognizedCommand = new Command();
-            bool commandFound = false;
+            if (LoadedCommands == null)
+            {
+                Log.LogToConsole("Cannot execute commands when no commands are loaded!", Log.LogType.Warning);
+                return;
+            }
+
+            Command? recognizedCommand = null;
 
             foreach (var command in LoadedCommands)
             {
                 if (command.CommandPhrase == result)
                 {
                     recognizedCommand = command;
-                    commandFound = true;
                     break;
                 }
             }
 
-            if (commandFound) // Only use recognizedCommand when a command has actually been recognized. Can't check for null on recognizedCommand because it's a (non-nullable) struct
+            if (recognizedCommand != null) // Only use recognizedCommand when a command has actually been recognized. Can't check for null on recognizedCommand because it's a (non-nullable) struct
             {
-                Log.LogToConsole($"Command recognized \"{recognizedCommand.CommandPhrase}\"", Log.LogType.Info);
-                Keyboard.SendInputs(recognizedCommand.Inputs);
+                Log.LogToConsole($"Command recognized \"{recognizedCommand.Value.CommandPhrase}\"");
+                Keyboard.SendInputs(recognizedCommand.Value.Inputs);
             }
         }
 
         private static void ShutDown()
         {
-            Log.LogToConsole("Shutdown phrase recognized, shutting down...", Log.LogType.Info);
+            Log.LogToConsole("Shutdown phrase recognized, shutting down...");
             Thread.Sleep(1000);
-            Completed.Set();
+            Completed?.Set();
         }
 
-        private static void OnSpeechRecognitionRejected(object sender, SpeechRecognitionRejectedEventArgs args) => Log.LogToConsole("Still listening...", Log.LogType.Info);
+        private static void OnSpeechRecognitionRejected(object? sender, SpeechRecognitionRejectedEventArgs args) => Log.LogToConsole("Still listening...");
     }
 }
