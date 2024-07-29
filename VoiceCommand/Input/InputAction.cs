e0a889c97040842ff0bd1e508447f775c725a6fa
Sjouke IJntema <18188660+DevCas1@users.noreply.github.com>
2024-07-30 01:03:55 +0200

    Small refactor

--- VoiceCommand/Input/InputAction.cs
+++ VoiceCommand/Input/InputAction.cs
@@ -4,11 +4,9 @@ namespace VoiceCommand.Input;
 /// <param name="scanCode">Indicates which key to press or release.</param>
 /// <param name="keyDown">True to press key, false to release.<br/>An identical keyDown value following the same <see cref="ScanCode"/> could have unintended effects and isn't tested!</param>
 /// <param name="duration">The duration in miliseconds before this Input Action is considered done.</param>
-public readonly struct InputAction(ScanCode scanCode, bool keyDown, float duration)
+public readonly struct InputAction(ScanCode scanCode, bool keyDown, float duration = 0)
 {
     public readonly ScanCode Scancode = scanCode;
     public readonly bool KeyDown = keyDown;
     public readonly float Duration = duration;
-
-    public InputAction(ScanCode scanCode, bool keyDown) : this(scanCode, keyDown, 0) { }
 }
